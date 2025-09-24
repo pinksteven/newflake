434b70b04f0133f385312de6f08f50ba4e761068
Steven <st.latuszek@gmail.com>
2025-09-24 14:09:19 +0200

    launch regreet via cage

--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -1,39 +1,11 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}: let
-  hyprland-config = pkgs.writeText "greetd.conf" ''
-    exec-once = regreet; hyprctl dispatch exit
-    misc {
-        disable_hyprland_logo = true
-        disable_splash_rendering = true
-        disable_hyprland_qtutils_check = true
-    }
-    animations {
-      enabled = false
-    }
-  '';
-in {
-  users.extraUsers.greeter = {
-    # For caching and such
-    home = "/tmp/greeter-home";
-    createHome = true;
-  };
-
   services.greetd = {
     enable = true;
-    settings = {
-      default_session = {
-        command = "${lib.getExe pkgs.hyprland} --config ${hyprland-config}";
-        user = config.users.extraUsers.greeter.name;
-      };
-    };
   };
 
   programs.regreet = {
     enable = true;
+    cageArgs = ["-s" "-m" "last"];
   };
 
   environment.persistence = {
