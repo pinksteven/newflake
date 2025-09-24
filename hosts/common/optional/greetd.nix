d3eb46964e10ab3f9fe0a4042c2983359347121d
Steven <st.latuszek@gmail.com>
2025-09-24 13:51:31 +0200

    move to regreet

--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -4,9 +4,17 @@
   pkgs,
   ...
 }: let
-  theme = ''border=blue;text=yellow;prompt=green;action=cyan;button=magenta;container=black;input=white'';
-  settings = "-r --asterisks --asterisks-char • --window-padding 1 -t
-              --time-format '%H:%M | %a  %d %b %Y' -g 'Welcome back!' --remember-user-session";
+  hyprland-config = pkgs.writeText "greetd.conf" ''
+    exec-once = regreet; hyprctl dispatch exit
+    misc {
+        disable_hyprland_logo = true
+        disable_splash_rendering = true
+        disable_hyprland_qtutils_check = true
+    }
+    animations {
+      enabled = false
+    }
+  '';
 in {
   users.extraUsers.greeter = {
     # For caching and such
@@ -18,10 +26,25 @@ in {
     enable = true;
     settings = {
       default_session = {
-        command = "${lib.getExe pkgs.tuigreet} ${settings} --theme '${theme}'";
+        command = "${lib.getExe pkgs.hyprland} --config ${hyprland-config}";
         user = config.users.extraUsers.greeter.name;
       };
     };
+  };
+
+  programs.regreet = {
+    enable = true;
+  };
+
+  environment.persistence = {
+    # Persist last user and last selected session
+    "/persist".directories = [
+      {
+        directory = "/var/lib/regreet";
+        user = "greeter";
+        group = "greeter";
+      }
+    ];
   };
 
   security.pam.services.greetd.fprintAuth = false;
