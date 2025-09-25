cda75c16ebad5714d3100118d3f31792ad94102c
PinkSteven <st.latuszek@gmail.com>
2025-09-25 11:40:31 +0200

    focus primaryMonitor on Hyprland startup

--- home/steven/features/hyprland/startup.nix
+++ home/steven/features/hyprland/startup.nix
@@ -1,1 +1,11 @@
-{}
+{
+  monitors,
+  lib,
+  ...
+}: {
+  wayland.windowManager.hyprland.settings.exec-once = let
+    primaryMonitor = lib.head (lib.filter (m: m.primary) monitors);
+  in [
+    "hyprctl dispath focusmonitor ${primaryMonitor.name}"
+  ];
+}
