a15845774593b0ab9ab8ef109606e9c37a2ce2d1
PinkSteven <st.latuszek@gmail.com>
2025-09-25 17:03:35 +0200

    style spicetify with stylix

--- home/steven/features/base-apps/spicetify.nix
+++ home/steven/features/base-apps/spicetify.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   inputs,
-  config,
   ...
 }: {
   home.persistence."/persist/home/steven" = {
@@ -11,7 +10,6 @@
     ];
   };
 
-  stylix.targets.spicetify.enable = false;
   imports = [inputs.spicetify.homeManagerModules.default];
 
   programs.spicetify = let
@@ -21,38 +19,6 @@
     experimentalFeatures = true;
     wayland = true;
     windowManagerPatch = true;
-    theme = spice.themes.hazy;
-
-    # colorScheme = "Violet";
-
-    customColorScheme = with config.lib.stylix.colors; {
-      text = base05;
-      subtext = base04;
-      main = base01;
-      main-elevated = base02;
-      main-transition = base01;
-      highlight = base07;
-      highlight-elevated = base0D;
-      sidebar = base00;
-      player = base01;
-      card = base07;
-      shadow = base07;
-      selected-row = base05;
-      button = base0E;
-      button-active = base0E;
-      button-disabled = base03;
-      tab-active = base07;
-      notification = base0C;
-      notification-error = base08;
-      misc = base05;
-      play-button = base0D;
-      play-button-active = base0D;
-      progress-fg = base0D;
-      progress-bg = base02;
-      heart = base08;
-      pagelink-active = base0A;
-      radio-btn-active = base0C;
-    };
 
     enabledExtensions = with spice.extensions; [
       fullAppDisplay
