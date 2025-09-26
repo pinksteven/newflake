f4dc7f680a7e0894465a43df7b8d8041fba8ce74
Steven <st.latuszek@gmail.com>
2025-09-26 23:22:16 +0200

    dnd: add maptool

--- home/steven/features/dnd/default.nix
+++ home/steven/features/dnd/default.nix
@@ -2,12 +2,14 @@
   home.packages = with pkgs; [
     loreforge
     dungeondraft
+    maptool
   ];
 
   home.persistence."/persist/home/steven" = {
     directories = [
       ".config/lore-forge"
       ".local/share/Dungeondraft"
+      ".maptool"
     ];
   };
 }
