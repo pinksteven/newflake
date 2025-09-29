4b6b7c7c2c0c70571b9a5230f698b7298553a3e2
PinkSteven <st.latuszek@gmail.com>
2025-09-29 10:27:55 +0200

    zed: set userSettings via hm

--- home/steven/features/zed/default.nix
+++ home/steven/features/zed/default.nix
@@ -2,10 +2,46 @@
   programs.zed-editor = {
     enable = true;
 
-    extensions = ["nix"];
+    userSettings = {
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+      vim_mode = true;
+
+      languages = {
+        Nix.formatter.external = {
+          command = "alejandra";
+          arguments = [
+            "--quiet"
+            "--"
+          ];
+        };
+      };
+    };
+    # userTasks = {};
+    # userKeymaps = {};
+
+    extensions = [
+      "nix"
+      "toml"
+      "lua"
+      "neocmake"
+      "basher"
+      "discord-presence"
+      "wakatime"
+      "qml"
+      "snippets"
+      "colored-zed-icons-theme"
+    ];
     extraPackages = with pkgs; [
       nixd
       nil
+      alejandra
     ];
+  };
+
+  home = {
+    packages = with pkgs; [cloc];
   };
 }
