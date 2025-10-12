d22967623de9c9bc50873df2cffd3dd52a33b4ba
PinkSteven <st.latuszek@gmail.com>
2025-10-12 14:52:39 +0200

    niri: more bindings

--- home/steven/features/desktop/niri/binds.nix
+++ home/steven/features/desktop/niri/binds.nix
@@ -3,6 +3,8 @@
     dms-ipc = spawn "dms" "ipc";
   in {
     "Ctrl+Alt+Delete".action = quit;
+    "Mod+Shift+slash".action = show-hotkey-overlay;
+    "Mod+Tab".action = toggle-overview;
     "Mod+F".action = spawn "ghostty" "-e" "yazi";
     "Mod+B".action = spawn "firefox";
     "Mod+Q".action = close-window;
@@ -18,9 +20,27 @@
     "Shift+Mod+Right".action = move-column-right;
     "Shift+Mod+Up".action = move-window-up;
     "Shift+Mod+Down".action = move-window-down;
+    # Combine into column with Mod+Ctrl+arrow keys
+    "Ctrl+Mod+Left".action = consume-or-expel-window-left;
+    "Ctrl+Mod+Right".action = consume-or-expel-window-right;
     # Workspace navigation
     "Mod+Page_Up".action = focus-workspace-up;
     "Mod+Page_Down".action = focus-workspace-down;
+    "Mod+Shift+Page_Up".action = move-column-to-workspace-up;
+    "Mod+Shift+Page_Down".action = move-column-to-workspace-down;
+
+    # Column size management
+    "Mod+R".action = switch-preset-column-width;
+    "Shift+Mod+R".action = switch-preset-column-width-back;
+    "Ctrl+Mod+R".action = maximize-column;
+    "Mod+equal".action = set-column-width "+5%";
+    "Mod+minus".action = set-column-width "-5%";
+    "Shift+Mod+equal".action = set-window-height "+5%";
+    "Shift+Mod+minus".action = set-window-height "-5%";
+
+    # Tiling/floating management
+    "Alt+Tab".action = switch-focus-between-floating-and-tiling;
+    "Mod+T".action = toggle-window-floating;
 
     # Access media workspace (Spotify + Vesktop)
     "Mod+Shift+M".action = focus-workspace "media";
