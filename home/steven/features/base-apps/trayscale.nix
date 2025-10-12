f4ff349768b8b5f943f43f8a55d852ac2ab9e955
PinkSteven <st.latuszek@gmail.com>
2025-10-12 22:40:40 +0200

    trayscale: use startup hm module

--- home/steven/features/base-apps/trayscale.nix
+++ home/steven/features/base-apps/trayscale.nix
@@ -1,9 +1,10 @@
 {pkgs, ...}: {
-  services.trayscale = {
-    enable = true;
-    hideWindow = true;
-  };
-  # The trayscale module is wierd and doesn't "install" trayscale
-  # so it's not in path, and i don't like that
   home.packages = with pkgs; [trayscale];
+
+  startupPrograms = [
+    {
+      name = "trayscale";
+      command = ["trayscale" "--hide-window"];
+    }
+  ];
 }
