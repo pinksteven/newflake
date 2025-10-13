56076270495ebd4cc6547dd0c5382ebdbc9ae258
PinkSteven <st.latuszek@gmail.com>
2025-10-13 11:23:03 +0200

    zsh: fix zsh-abbr expansion in vi mode

--- home/steven/features/cli/zsh/default.nix
+++ home/steven/features/cli/zsh/default.nix
@@ -19,6 +19,12 @@
 
     initContent = lib.mkAfter ''
       bindkey -v
+
+
+      bindkey -M viins " " abbr-expand-and-insert
+      bindkey -M viins "^ " magic-space
+      bindkey -M viins "^M" abbr-expand-and-accept
+
       bindkey "^[[3~" delete-char
       microfetch
     '';
