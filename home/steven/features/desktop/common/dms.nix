c297c00d4d60385d8fffc2a8f9316da255457916
PinkSteven <st.latuszek@gmail.com>
2025-10-28 11:13:58 +0100

    flake update

--- home/steven/features/desktop/common/dms.nix
+++ home/steven/features/desktop/common/dms.nix
@@ -14,9 +14,7 @@
     enableSystemd = true;
     enableSystemMonitoring = false;
     enableClipboard = false; # I manage clipboard on my own
-    enableNightMode = false;
     enableDynamicTheming = false;
-    quickshell.package = inputs.dankMaterialShell.packages.${pkgs.system}.quickshell;
   };
   home.persistence."/persist/home/steven" = {
     directories = [
