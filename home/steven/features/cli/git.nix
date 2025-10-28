1d7c19d538de0a11b8d6dcac0e6a337d87dd0a49
PinkSteven <st.latuszek@gmail.com>
2025-10-28 09:27:04 +0100

    chase hm changes

--- home/steven/features/cli/git.nix
+++ home/steven/features/cli/git.nix
@@ -3,10 +3,12 @@
     enable = true;
     lfs.enable = true;
     package = pkgs.gitAndTools.gitFull;
-    userName = "PinkSteven";
-    userEmail = "st.latuszek@gmail.com";
 
-    extraConfig = {
+    settings = {
+      user = {
+        name = "PinkSteven";
+        email = "st.latuszek@gmail.com";
+      };
       init.defaultBranch = "main";
       push.default = "current";
       push.autoSetupRemote = true;
@@ -18,28 +20,29 @@
       branch.sort = "committerdate";
 
       rerere.enabled = true;
-    };
-    delta = {
-      enable = true;
-      # TODO: Setup themeing
+
+      alias = {
+        st = "status";
+        co = "checkout";
+        br = "branch";
+        cm = "commit -m";
+        amend = "commit --amend";
+        lg = "log --oneline --graph --all --decorate";
+        lgg = "log --oneline --graph --decorate --all --stat";
+        df = "diff";
+        dc = "diff --cached";
+        last = "log -1 HEAD";
+        unstage = "reset HEAD --";
+        aliases = "!git config -l | grep allowDiscards";
+      };
     };
     ignores = [
       ".direnv"
     ];
-    aliases = {
-      st = "status";
-      co = "checkout";
-      br = "branch";
-      cm = "commit -m";
-      amend = "commit --amend";
-      lg = "log --oneline --graph --all --decorate";
-      lgg = "log --oneline --graph --decorate --all --stat";
-      df = "diff";
-      dc = "diff --cached";
-      last = "log -1 HEAD";
-      unstage = "reset HEAD --";
-      aliases = "!git config -l | grep allowDiscards";
-    };
   };
   programs.lazygit.enable = true;
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+  };
 }
