15f54c679dcf739bab02781b5907768c0e69b2c9
PinkSteven <st.latuszek@gmail.com>
2026-02-25 13:22:19 +0100

    add services to kaermorhen

--- hosts/kaermorhen/default.nix
+++ hosts/kaermorhen/default.nix
@@ -16,10 +16,12 @@
     # ./services/couchdb.nix # For obisidian livesync
     # ./services/forgejo.nix
     # ./services/grafana.nix
+    # ./services/mc-server.nix
     # ./services/prometheus.nix
     # ./services/radicale.nix
     # ./services/tailscale-serve.nix
     # ./services/wakapi.nix
+    # ./services/wol.nix
   ];
 
   networking = {
---
+++ hosts/kaermorhen/services/mc-server.nix
@@ -0,0 +1,30 @@
+{
+  virtualisation.oci-containers.containers.mc-server = {
+    image = "itzg/minecraft-server:latest";
+    autoStart = true;
+
+    ports = ["25565:25565"];
+    volumes = ["/srv/mc-server:/data"];
+
+    environment = {
+      EULA = "TRUE";
+      TYPE = "MODRINTH";
+      MEMORY = "8192M";
+      MAX_PLAYERS = "10";
+      USE_MEOWICE_FLAGS = "true";
+      TZ = "Europe/Warsaw";
+      DIFFICULTY = "2";
+      SPAWN_PROTECTION = "0";
+      REGION_FILE_COMPRESSION = "lz4";
+      ENABLE_WHITELIST = "true";
+      ALLOW_FLIGHT = "true";
+      RCON_PASSWORD = "";
+    };
+
+    extraOptions = [
+      "--cpus=4"
+      "--cpu-shares=512"
+      "--memory=8g"
+    ];
+  };
+}
