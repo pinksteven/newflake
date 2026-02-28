8466c9ab2d89b3ea71dc64739499402796c34bec
PinkSteven <st.latuszek@gmail.com>
2026-02-28 18:11:20 +0100

    add wakapi salt

--- hosts/kaermorhen/services/prometheus.nix
+++ hosts/kaermorhen/services/prometheus.nix
@@ -5,7 +5,7 @@
     exporters = {
       node = {
         enable = true;
-        enabledCollectors = ["systemd"];
+        enabledCollectors = ["systemd" "processes"];
         port = 9002;
       };
     };
--- hosts/kaermorhen/services/wakapi.nix
+++ hosts/kaermorhen/services/wakapi.nix
@@ -1,5 +1,13 @@
-{
+{config, ...}: {
+  sops.secrets = {
+    "wakapi/salt" = {
+      mode = "0600";
+      owner = "wakapi";
+      sopsFile = ../secrets.yaml;
+    };
+  };
   services.wakapi = {
     enable = true;
+    passwordSaltFile = config.sops.secrets."wakapi/salt".path;
   };
 }
