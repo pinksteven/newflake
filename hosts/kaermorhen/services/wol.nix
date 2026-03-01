683e66a6e1f735baaab15c4edf99580f9652ed2c
PinkSteven <st.latuszek@gmail.com>
2026-03-01 15:25:12 +0100

    fix script

--- hosts/kaermorhen/services/wol.nix
+++ hosts/kaermorhen/services/wol.nix
@@ -8,9 +8,9 @@
     source ${config.sops.secrets.wol.path}
 
     TARGET="$1"
-    MAC="${!TARGET:-}"
+    MAC="$\{!TARGET:-}"
 
-    if [ -z "${MAC:-}" ]; then
+    if [ -z "$\{MAC:-}" ]; then
       echo "Unknown target"
       exit 1
     fi
