0b5c57b51f165a4368d04e4c940ae6f402ef9461
PinkSteven <st.latuszek@gmail.com>
2026-03-11 08:53:17 +0100

    rework builders

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -22,39 +22,62 @@
       sshKey = "/persist/etc/ssh/ssh_host_ed25519_key";
       protocol = "ssh-ng";
       maxJobs = hosts.${name}.config.builder.maxJobs;
-      speedFactor = hosts.${name}.config.builder.speedFactor;
+      speedFactor = lib.mkIf (!(config.builder.enable)) hosts.${name}.config.builder.speedFactor;
       supportedFeatures = ["nixos-test" "kvm" "big-parallel"];
     })
     (lib.filter (name: name != config.networking.hostName) builderHostNames);
 in {
-  nix = {
-    package = pkgs.lixPackageSets.latest.lix;
-    settings = {
-      extra-substituters = [];
-      trusted-public-keys = [];
+  config = {
+    nix = {
+      package = pkgs.lixPackageSets.latest.lix;
+      settings = {
+        substituters = ["https://cache.nixos-cuda.org"];
+        trusted-public-keys = ["cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="];
 
-      max-jobs = lib.mkDefault 1;
-      trusted-users = ["root" "@wheel" "@builders"];
-      auto-optimise-store = lib.mkDefault true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      warn-dirty = false;
-      system-features = [
-        "kvm"
-        "big-parallel"
-        "nixos-test"
-      ];
+        max-jobs = lib.mkDefault 1;
+        trusted-users = ["root" "@wheel" "@builders"];
+        auto-optimise-store = lib.mkDefault true;
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        warn-dirty = false;
+        system-features = [
+          "kvm"
+          "big-parallel"
+          "nixos-test"
+        ];
+      };
+      optimise = {
+        automatic = true;
+      };
+      distributedBuilds = lib.mkDefault true;
+      buildMachines = buildMachinesForThisHost;
+      extraOptions = ''
+        builders-use-substitutes = true
+      '';
     };
-    optimise = {
-      automatic = true;
-    };
-    distributedBuilds = lib.mkDefault true;
-    buildMachines = buildMachinesForThisHost;
-    extraOptions = ''
-      builders-use-substitutes = true
-    '';
+    programs.nix-ld.enable = true;
   };
-  programs.nix-ld.enable = true;
+
+  # Create builder option
+  options.builder = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether this machine should be used as a remote Nix build machine.";
+    };
+
+    maxJobs = lib.mkOption {
+      type = lib.types.int;
+      default = 4;
+      description = "Maximum number of jobs this builder should advertise to remote clients.";
+    };
+
+    speedFactor = lib.mkOption {
+      type = lib.types.int;
+      default = 10;
+      description = "Relative speed factor advertised to remote clients.";
+    };
+  };
 }
--- hosts/common/optional/builder.nix
+++ hosts/common/optional/builder.nix
@@ -6,29 +6,10 @@
   ...
 }: let
   hosts = lib.attrNames outputs.nixosConfigurations;
-  cfg = config.builder;
 in {
-  options.builder = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Whether this machine should be used as a remote Nix build machine.";
-    };
+  config = {
+    builder.enable = true;
 
-    maxJobs = lib.mkOption {
-      type = lib.types.int;
-      default = 4;
-      description = "Maximum number of jobs this builder should advertise to remote clients.";
-    };
-
-    speedFactor = lib.mkOption {
-      type = lib.types.int;
-      default = 20;
-      description = "Relative speed factor advertised to remote clients.";
-    };
-  };
-
-  config = lib.mkIf cfg.enable {
     users.groups.builders = {};
 
     users.users.builder = {
@@ -42,7 +23,6 @@ in {
 
     nix.settings = {
       max-jobs = "auto";
-      speedFactor = cfg.speedFactor;
     };
   };
 }
