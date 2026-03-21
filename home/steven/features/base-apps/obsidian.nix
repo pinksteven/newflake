34db8829cfd6766e538b4088931b23c4b610d74f
PinkSteven <st.latuszek@gmail.com>
2026-03-21 13:00:30 +0100

    obsidian: create config

--- home/steven/features/base-apps/obsidian.nix
+++ home/steven/features/base-apps/obsidian.nix
@@ -1,3 +1,22 @@
-{pkgs, ...}: {
-  home.packages = [pkgs.obsidian];
+{
+  programs.obsidian = {
+    enable = true;
+    vaults = {
+      uni = {
+        target = "Documents/.obsidian/uni";
+      };
+      victorian = {
+        target = "Documents/.obsidian/victorian";
+      };
+    };
+  };
+  stylix.targets.obsidian = {
+    enable = true;
+    colors.enable = true;
+    fonts.enable = true;
+    vaultNames = ["uni" "victorian"];
+  };
+  home.persistence."/persist" = {
+    directories = [".config/obsidian"];
+  };
 }
