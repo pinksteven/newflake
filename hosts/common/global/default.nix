09cdd102cb0aa882606636cb21b6dea8b61bb407
PinkSteven <st.latuszek@gmail.com>
2026-04-15 13:20:39 +0200

    Added fonts

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -58,6 +58,13 @@
   fonts = {
     enableDefaultPackages = true;
     enableGhostscriptFonts = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      liberation_ttf
+      dejavu_fonts
+    ];
   };
 
   security.pam.services.login = {
