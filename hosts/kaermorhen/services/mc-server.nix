998ceda01a8c3e6c7306c6b468682c73cfe2fb77
PinkSteven <st.latuszek@gmail.com>
2026-08-10 15:39:34 +0200

    vanilla tweaks code fix

--- hosts/kaermorhen/services/mc-server.nix
+++ hosts/kaermorhen/services/mc-server.nix
@@ -29,7 +29,7 @@
       REGION_FILE_COMPRESSION = "lz4";
       ENABLE_WHITELIST = "true";
       USE_SIMD_FLAGS = "true";
-      VANILLATWEAKS_SHARECODE = "NjMc2u";
+      VANILLATWEAKS_SHARECODE = "OfrQme";
       ALLOW_FLIGHT = "true";
       MOTD = "\\u00a7l   \\u00a7r           \\u00a76\\u00a7l\\u00a7kbuh\\u00a7d Greetings traveler! \\u00a76\\u00a7l\\u00a7kbuh\\u00a7r\\n\\u00a7l   \\u00a7r @me on discord for whitelist if you can't join";
     };
